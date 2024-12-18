fee3c2b71a3c45f296e0b4a0800efc8f549618cd
anu <anu@192.168.0.3>
2024-12-18 18:09:57 +0530

    commit --dev1

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -1,10 +1,13 @@
 package com.nt.main;
 
-/**
- * Hello world!
- */
 public class App {
+	public int sum(int x,int y) {
+		return x+y;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        App ap=new App();
+        int result=ap.sum(10,20);
+        System.out.println("sum is::"+result);
     }
 }
--- Proj01/src/test/java/com/nt/main/AppTest.java
+++ Proj01/src/test/java/com/nt/main/AppTest.java
@@ -1,19 +1,59 @@
 package com.nt.main;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+	private static App ap;
+	@BeforeAll
+	public static void setupOnce() {
+		ap=new App();
+	}
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumWithPositives() {
+    	int x=10;
+    	int y=20;
+    	int expected=30;
+    	int actual=ap.sum(x, y);
+    	assertEquals(expected,actual);
+       
+    }
+    @Test
+    public void testSumWithNegitives() {
+    	int x=-10;
+    	int y=-20;
+    	int expected=-30;
+    	int actual=ap.sum(x, y);
+    	assertEquals(expected,actual);
+       
+    }
+    @Test
+    public void testSumWithMixed() {
+    	int x=-10;
+    	int y=20;
+    	int expected=10;
+    	int actual=ap.sum(x, y);
+    	assertEquals(expected,actual);
+       
+    }
+
+	/*
+	 * @Test public void testSumWithZeroes() { int x=0; int y=0; int expected=0; int
+	 * actual=ap.sum(x, y); assertEquals(expected,actual);
+	 * 
+	 * }
+	 * 
+	 * @Test public void testSumWithNonZeroes() { int x=0; int y=1; int expected=1;
+	 * int actual=ap.sum(x, y); assertEquals(expected,actual);
+	 * 
+	 * }
+	 */
+    @AfterAll
+    public static void clearOnce() {
+    	ap=null;
     }
 }
