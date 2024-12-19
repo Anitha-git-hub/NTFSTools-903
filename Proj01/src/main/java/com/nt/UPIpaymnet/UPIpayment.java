7e821a0ee1005f3da23304dced97cf734a5c6097
anu <anu@192.168.0.5>
2024-12-19 06:52:00 +0530

    improved upi by TL

--- Proj01/src/main/java/com/nt/UPIpaymnet/UPIpayment.java
+++ Proj01/src/main/java/com/nt/UPIpaymnet/UPIpayment.java
@@ -5,4 +5,8 @@ public class UPIpayment {
 	public String withdrawl(String acno, long amnt) {
 		return amnt+"is withdrawl successfully from the"+acno;
 	}
+	public String deposit(String acno, long amnt) {
+		return amnt+"is deposited successfully into the"+acno;
+	}
+	
 }
