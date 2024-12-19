939a5a3a42a8ffa39e775cd674de499fd23f0dc0
anu <anu@192.168.0.5>
2024-12-19 06:33:57 +0530

    commit by --TL

--- Proj01/src/main/java/com/nt/main/App.java
+++ Proj01/src/main/java/com/nt/main/App.java
@@ -4,6 +4,9 @@ public class App {
 	public int sum(int x,int y) {
 		return x+y;
 	}
+	public int div(int x,int y) {
+		return x-y;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
         App ap=new App();
