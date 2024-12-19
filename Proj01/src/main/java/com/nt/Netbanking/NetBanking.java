73754bb2d984884f1fa1610fb79ecd9beda0891e
Team Leader <TL@192.168.0.5>
2024-12-19 07:08:31 +0530

    improved again netbanking by TL

--- Proj01/src/main/java/com/nt/Netbanking/NetBanking.java
+++ Proj01/src/main/java/com/nt/Netbanking/NetBanking.java
@@ -8,6 +8,12 @@ public class NetBanking {
 	public String transferMoney(String srcAcno, String destAcno, long amnt) {
 		return amnt+" money trasferred from "+srcAcno+" to "+destAcno;
 	}
+	public String receiveMoney(String destAcno, String srcAcno, long amnt) {
+		return amnt+" money received from "+destAcno+" to "+srcAcno;
+	}
+	public String closeAccount() {
+		return "account closed successfully";
+	}
 	
 
 }
