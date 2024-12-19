3cf37a723c94706e96f707824ec4fe5884bd3dd0
Team Leader <TL@192.168.0.5>
2024-12-19 07:05:18 +0530

    inprove netbanking by dev1

--- Proj01/src/main/java/com/nt/Netbanking/NetBanking.java
+++ Proj01/src/main/java/com/nt/Netbanking/NetBanking.java
@@ -1,8 +1,13 @@
 package com.nt.Netbanking;
 
 public class NetBanking {
+	
+	public String accountOpen(String full, String aadharNo, String contact) {
+		return "account created successfully";
+	}
 	public String transferMoney(String srcAcno, String destAcno, long amnt) {
 		return amnt+" money trasferred from "+srcAcno+" to "+destAcno;
 	}
+	
 
 }
