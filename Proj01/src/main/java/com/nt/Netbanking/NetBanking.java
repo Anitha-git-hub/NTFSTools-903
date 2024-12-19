07d1b9cba6d1fcdf45d8599b5c20073bf6298ce7
Team Leader <TL@192.168.0.5>
2024-12-19 07:00:35 +0530

    commit netbanking by TL

---
+++ Proj01/src/main/java/com/nt/Netbanking/NetBanking.java
@@ -0,0 +1,8 @@
+package com.nt.Netbanking;
+
+public class NetBanking {
+	public String transferMoney(String srcAcno, String destAcno, long amnt) {
+		return amnt+" money trasferred from "+srcAcno+" to "+destAcno;
+	}
+
+}
